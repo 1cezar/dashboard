b66d0528185ad368206a8b89f96b51581d9579d6
Carlos Sarubi <63349628+sarubit@users.noreply.github.com>
2024-03-23 19:54:35 +0000

    23/03

--- dashboard.py
+++ dashboard.py
@@ -7,93 +7,111 @@ import numpy as np
 import plotly.graph_objects as go
 import streamlit.components.v1 as components
 
-data = pd.read_csv('./Dados_Sudeste-Centro-Oeste_mensal.csv')
-data['DATA'] = pd.to_datetime(data['DATA'])
+# Função para carregar e processar os dados
+def load_data():
+    data = pd.read_csv('./Dados/Dados_Sudeste-Centro-Oeste_mensal.csv')
+    data['DATA'] = pd.to_datetime(data['DATA'])
+    correlation_matrix = data.corr(method='pearson')
+    return data, correlation_matrix
 
-# Calcular correlações usando o método de Pearson
-correlation_matrix = data.corr(method='pearson')
+# Função principal da página 1
+def pagina_um():
+    st.title("Análise de Dados de Energia")
 
-# Configurações de estilo
-st.set_page_config(
-    page_title="Análise de Dados de Energia",
-    page_icon=":bar_chart:",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+    # Carregar os dados e calcular correlações
+    data, correlation_matrix = load_data()
 
-# Layout do aplicativo
-st.title("Análise de Dados de Energia")
+    st.markdown("""
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    """, unsafe_allow_html=True)
 
-# Layout do gráfico
-col1, col2 = st.columns([1, 8])
-with col1:
-    prazo = st.radio("Prazo:", ("Trimestral", "Longo prazo"))
+    # Layout do gráfico
+    col1, col2 = st.columns([1, 8])
 
-# Determinar a variável base com base na seleção do interruptor
-if prazo == "Trimestral":
-    base_variable = "MPC_SECO"
-    selected_label = "Trimestral"
-else:
-    base_variable = "LPC_SECO"
-    selected_label = "Longo prazo"
+    with col1:
+        prazo = st.radio("Prazo:", ("Trimestral", "Longo prazo"), key='prazo')
 
-# Dropdown para selecionar as variáveis para correlacionar com a variável base
-with col2:
-    selected_variables = st.multiselect(f"Selecionar variáveis para correlacionar com {selected_label}:", data.columns[1:], default=[data.columns[2]])
+    # Determinar a variável base com base na seleção do interruptor
+    if prazo == "Trimestral":
+        base_variable = "MPC_SECO"
+        selected_label = "Trimestral"
+    else:
+        base_variable = "LPC_SECO"
+        selected_label = "Longo prazo"
 
-# Verificar se alguma variável foi selecionada
-if selected_variables:
-    # Adicionar variável base à seleção
-    selected_variables.append(base_variable)
+    # Criar um dicionário de mapeamento de variáveis
+    variavel_map = {
+        "Carga": "CE_SECO",
+        "Custo Marginal de Operação":"CMO_SECO",
+        "Energia Armazenada": "EA_SECO",
+        "Geração de Energia": "GE_SECO",
+        "Demanda Máxima": "DM_SECO",
+        "Preço da Liquidação das Diferenças": "PLD_SECO",
+        "Capacidade Instalada": "CI_SECO",
+        "PIB": "PIB"
+        # Adicione mais mapeamentos conforme necessário
+    }
 
-    # Filtrar os dados para incluir apenas as variáveis selecionadas
-    filtered_data = data[['DATA'] + selected_variables]
+    # Dropdown para selecionar a variável para correlacionar com a variável base
+    with col2:
+        selected_variable = st.selectbox(f"Selecionar variável para correlacionar com {selected_label}:", list(variavel_map.keys()), index=1)
 
-    # Gráfico de série temporal para as variáveis selecionadas
-    fig_time_series = px.line(filtered_data, x='DATA', y=selected_variables, 
-                              title=f"Série Temporal das Variáveis Selecionadas para {selected_label}", 
-                              labels={'variable': 'Variável', 'value': 'Valor (MW)'}, template='plotly_white')
+    # Verificar se uma variável foi selecionada
+    if selected_variable:
+        # Obter o nome da variável correspondente
+        selected_variable_corresponding = variavel_map[selected_variable]
 
-    # Adicionar eixo y à direita
-    for i in range(1, len(selected_variables)):
-        fig_time_series.update_traces(yaxis=f"y{i + 1}", selector=dict(name=selected_variables[i]))
+        # Adicionar variável base à seleção
+        selected_variables = [selected_variable_corresponding, base_variable]
 
-    # Atualizar layout para incluir segundo eixo y à direita
-    for i in range(2, len(selected_variables) + 1):
-        fig_time_series.update_layout(yaxis2=dict(anchor='x', overlaying='y', side='right', position=0.95 - 0.05 * (i - 1)), showlegend=True)
-    
-    # Calcular e exibir a correlação de Pearson entre as variáveis selecionadas e a variável base
-    for variable in selected_variables:
-        if variable != base_variable:
-            correlation_value = correlation_matrix.loc[base_variable, variable]
-            # Adicionar botões de alternância dentro do gráfico
-            fig_time_series.add_annotation(
-                x=0.5,
-                y=1.0,
-                opacity=0.8,
-                xref="paper",
-                yref="paper",
-                text=f"Correlação: {correlation_value:.2f}",
-                showarrow=False,
-                bgcolor="Green" if correlation_value >= 0.0 else "Red",
-                font=dict(size=25, color="white", family="monospace"),
-            )
+        # Filtrar os dados para incluir apenas as variáveis selecionadas
+        filtered_data = data[['DATA'] + selected_variables]
 
-    st.plotly_chart(fig_time_series, use_container_width=True)
-   
+        preco = ["Custo Marginal de Operação", "PIB", "Preço da Liquidação das Diferenças", "LPC_SECO", "MPC_SECO" ]
 
-    # Definir o código JavaScript
-    javascript_code = """
-    <h2>Exemplo de Integração JavaScript</h2>
-    <p>Clique no botão abaixo para exibir um alerta em JavaScript.</p>
-    <button onclick="showAlert()">Mostrar Alerta</button>
+        if selected_variable in preco:
+            grandesa = 'VALOR (R$)' 
 
-    <script>
-        function showAlert() {
-            alert("Este é um alerta em JavaScript!");
-        }
-    </script>
-    """
+        else:
+            grandesa = 'VALOR (MW)'
+            # Gráfico de série temporal para as variáveis selecionadas
+        fig_time_series = px.line(filtered_data, x='DATA', y=selected_variables, 
+                                title=f"Série Temporal das Variáveis Selecionadas para {selected_label}", 
+                                labels={'variable': 'Variável', 'value': grandesa}, template='plotly_white')
 
-    # Renderizar o componente de JavaScript
-    components.html(javascript_code)
+        # Definir as cores das linhas
+        fig_time_series.update_traces(line=dict(color='#1564c0'))  # Cor da primeira linha
+        if len(selected_variables) > 1:
+            fig_time_series.update_traces(line=dict(color='#398e3d'), selector=dict(name=selected_variables[1]))  # Cor da segunda linha, se houver
+
+        # Adicionar eixo y à direita
+        for i in range(1, len(selected_variables)):
+            fig_time_series.update_traces(yaxis=f"y{i + 1}", selector=dict(name=selected_variables[i]))
+
+        # Atualizar layout para incluir segundo eixo y à direita
+        for i in range(2, len(selected_variables) + 1):
+            fig_time_series.update_layout(yaxis2=dict(anchor='x', overlaying='y', side='right', position=0.95 - 0.05 * (i - 1), tickfont=dict(color='white')), showlegend=True)  # Alterado para preto
+
+        # Calcular e exibir a correlação de Pearson entre as variáveis selecionadas e a variável base
+        for variable in selected_variables:
+            if variable != base_variable:
+                correlation_value = correlation_matrix.loc[base_variable, variable]
+        st.plotly_chart(fig_time_series, use_container_width=True)
+
+        cor_color = "#398e3d" if correlation_value >= 0.0 else "#fb4d4c"
+        formatted_correlation = "{:.8f}".format(correlation_value)
+
+        st.metric(label="Correlação", value= formatted_correlation, delta=0, delta_color="inverse")
+
+
+# Barra de navegação
+st.sidebar.title("Navegação")
+pagina_selecionada = st.sidebar.radio("Selecione a página", ["Página 1", "Página 2"])
+
+def pagina_dois():
+    print('Oi')
+
+if pagina_selecionada == "Página 1":
+    pagina_um()
+elif pagina_selecionada == "Página 2":
+    pagina_dois()
